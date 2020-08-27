413567f9f8beeb1a8f06b3ef01c04cb31cacc18d
paulangel <paulangel@users.noreply.github.com>
2020-08-27 15:28:05 +1000

    Update example.py

--- example.py
+++ example.py
@@ -8,20 +8,21 @@ import scipy
 data           = pd.read_csv('/Users/pwangel/Downloads/myeloid_atlas_expression_v7.1.tsv', sep='\t', index_col=0)
 previous_genes = pd.read_csv('/Users/pwangel/PlotlyWorkspace/combine_data/blood/outputs_for_front_end/iMac_genes.tsv', sep='\t', index_col=0)
 annotations    = pd.read_csv('/Users/pwangel/PlotlyWorkspace/combine_data/blood/outputs_for_front_end/iMac_annotations.tsv', sep='\t', index_col=0)
+annotations['Dataset'] = [i.split(';') for i in annotations.index.values.astype(str)]
+
+annotations = annotations.loc[annotations.Dataset!='6638'] #### Do this if you want to get rid of dataset 6638
+data        = data[annotations.index]
 
 data = functions.transform_to_percentile(data)
 
-data = pd.DataFrame(scipy.stats.norm.ppf(data.values), index=data.index, columns=data.columns)
+genes = functions.calculate_platform_dependence(data, annotations)
 
-#genes = functions.calculate_platform_dependence(data, annotations)
+functions.plot_atlas(data, genes, annoations, '/Users/pwangel/Downloads')
 
-genes = functions.calculate_celltype_dependence(data, annotations)
 
-print(stop)
+#KW_Htest_results = functions.plot_KW_Htest(data, annotations, genes, '/Users/pwangel/Downloads/')
 
-KW_Htest_results = functions.plot_KW_Htest(data, annotations, genes, '/Users/pwangel/Downloads/')
-
-functions.plot_gene_platform_dependence(data, annotations, genes, '/Users/pwangel/Downloads/')
+#functions.plot_gene_platform_dependence(data, annotations, genes, '/Users/pwangel/Downloads/')
 
 #H_index_list, retained_genes_list = functions.resample_clustering(data, annotations, resample_strategy='bootstrap', n_resamples=2, n_clusters_list=[3,4])
 
